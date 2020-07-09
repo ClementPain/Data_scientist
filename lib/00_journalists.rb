93c820a0ee27b310ad8df664b9b5831dc7e381ff
ClementPain <clementpain@outlook.com>
2020-07-09 19:42:31 +0200

    version 1

---
+++ lib/00_journalists.rb
@@ -0,0 +1,171 @@
+handle=["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+
+
+# Combien y a-t-il de handle dans cette array ?
+
+def question1(array)
+	puts "il y a #{array.length} handles dans la liste"
+end
+
+#Quelle est le handle le plus court de cette liste ?
+
+def question2(array)
+	length_mail = array[0].length+1
+	shortest_mail = ""
+
+	array.each do |mail|
+
+		if length_mail > mail.length
+			shortest_mail = mail
+			length_mail=mail.length
+		end
+	end
+
+	puts "Le handle le plus court de la liste est #{shortest_mail}"
+end
+
+#Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+
+def question3(array)
+
+	puts "Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)"
+	print "> "
+
+	five_car=0
+
+	array.each do |mail|
+		if mail.length == 6
+			five_car+=1
+		end
+	end
+
+	puts five_car
+end
+
+
+#Combien commencent par une majuscule (première lettre juste après le @) ?
+
+def question4(array)
+
+	uppercase=0
+
+	array.each do |mail|
+		if mail[1] == mail[1].upcase
+			uppercase +=1
+		end
+	end
+
+	puts "Il y a #{uppercase} handle commençant par une majuscule"
+end
+
+#Trie la liste de handle par ordre alphabétique.
+
+def question5(array)
+	array.sort {|w1,w2| w1.casecmp(w2)}
+	puts "La liste a été triée par ordre alphabétique"
+end
+
+#Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+
+def question6(array)
+	array.sort_by(&:length)
+	puts "La liste a été triée par taille des handles"
+end
+
+#Quelle est la position dans l'array de la personne @epenser ?
+
+def question7(array)
+	puts "La personne @epenser est en position #{array.index("@epenser")} du tableau"
+end
+
+#Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+def question8(array)
+	size_mail= nb_handle_per_size(max_length(array), array)
+	puts "Les handles de quelle taille voulez-vous compter ?"
+	print "> "
+	nb_user=Integer(gets)
+	if nb_user<=max_length(array)
+		puts "Il y a #{size_mail[nb_user+1]} handles de taille #{nb_user}"
+	else
+		puts "Il n'y a pas de handles de plus de #{max_length(array)} caractères."
+		puts size_mail
+	end
+end
+
+def max_length(array)
+	max_length_mail=1
+
+	array.each do |mail|
+		if max_length_mail < mail.length
+			max_length_mail=mail.length
+		end
+	end
+	return max_length_mail
+end
+
+def nb_handle_per_size(max_length, array)
+	
+	arr_mail_length =[]
+	mail_length = 0
+
+	(max_length+1).times do
+		nb_mail = 0
+
+		array.each do |mail|
+			if mail.length == mail_length
+				nb_mail+=1
+			end
+		end
+
+		arr_mail_length<<nb_mail
+		mail_length +=1
+	end
+	return arr_mail_length
+end
+
+
+
+
+
+while true
+	puts "Bonjour ! Quelle est votre question ?"
+	puts "
+    1. Combien y a-t-il de handle dans cette array ?
+    2. Quelle est le handle le plus court de cette liste ?
+    3. Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+    4. Combien commencent par une majuscule (première lettre juste après le @) ?
+    5. Trie la liste de handle par ordre alphabétique.
+    6. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+    7. Quelle est la position dans l'array de la personne @epenser ?
+    8. Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)"
+
+ 	puts 
+ 	print "Numéro de question > "
+ 	choice_user=gets.chomp.to_i
+ 	puts
+
+ 	if choice_user==1
+ 		question1(handle)
+ 	elsif choice_user==2
+ 		question2(handle)
+ 	elsif choice_user==3
+ 		question3(handle)
+ 	elsif choice_user==4
+ 		question4(handle)
+ 	elsif choice_user==5
+ 		question5(handle)
+ 	elsif choice_user==6
+ 		question6(handle)
+ 	elsif choice_user==7
+ 		question7(handle)
+ 	elsif choice_user==8
+ 		question8(handle)
+ 	else
+ 		puts "Veuillez saisir un nombre entre 1 et 8"
+ 	end
+ 	puts	
+ 	puts "Appuyer sur 'entrée' pour poser une nouvelle question"
+ 	gets.chomp
+ end
